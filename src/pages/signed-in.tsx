34ee11adcf1bed0f76e58f4cfb55cdc7fc2fe4e8
oscar12844 <75006156+oscar12844@users.noreply.github.com>
2023-02-15 22:02:02 +0800

    update

--- src/pages/signed-in.tsx
+++ src/pages/signed-in.tsx
@@ -1,45 +1,49 @@
-import {GetServerSideProps, InferGetServerSidePropsType} from "next";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { json } from "stream/consumers";
+import { useRouter } from "next/router";
 
-
-
-
-export default function SignedIn({ joke }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const temp = JSON.stringify(joke.data[0].punchline)
+export default function SignedIn({
+  joke,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const temp = JSON.stringify(joke.data[0].punchline);
+  const router = useRouter();
+  function Home() {
+    return router.push("/");
+  }
   return (
     <div>
       <h1>Signed In</h1>
       {/* Task 3: Your own presentation of the joke here (Free Style 😉 )*/}
       <p id="showans">{joke.data[0].setup}</p>
-      <button onClick={()=> (alert(temp))}>Haha</button>
+      <button onClick={() => alert(temp)}>Haha</button>
       <div>
-        <button type="button" onClick={()=>window.location.reload()}>
-        Next Joke?
+        <button type="button" onClick={() => window.location.reload()}>
+          Next Joke?
         </button>
+        <div>
+          <button type="button" onClick={() => Home()}>
+            Home Page
+          </button>
+        </div>
       </div>
 
-
-
-
-      
       {/* End of Task 3 */}
     </div>
-  )
-
+  );
 }
 
 // Task 2: Fetch random jokes from the API
 // https://official-joke-api.appspot.com/jokes/programming/random
 export const getServerSideProps: GetServerSideProps = async (context) => {
   // Fetch data from external API and pass it to the page via props.joke
-  const res = await fetch('https://official-joke-api.appspot.com/jokes/programming/random');
+  const res = await fetch(
+    "https://official-joke-api.appspot.com/jokes/programming/random"
+  );
   const data = await res.json();
-  
+
   return {
     props: {
-      joke: {data}
-
-      ,
+      joke: { data },
     }, // will be passed to the page component as props
-  }
-}
+  };
+};
